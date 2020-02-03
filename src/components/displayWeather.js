86b7e142c4accb8ca4c67018b3e3155da29d0d1f
Stephen Ochieng <stephenojwang1040@gmail.com>
2020-02-03 22:59:09 +0300

    handling states

--- src/components/displayWeather.js
+++ src/components/displayWeather.js
@@ -1,42 +1,45 @@
-import React, { useState, useEffect } from "react";
-import CurrentWeather from "./currentPosition";
+import React, { Component } from 'react';
 
-const Weather = () => {
-  const [hasError, setErrors] = useState(false);
-  const [weather, setWeather] = useState({});
+class Weather extends Component {
+  constructor(props){
+    super(props);
 
-  const weatherData = JSON.stringify(weather.daily);
-  console.log(weatherData);
-
-
-  async function fetchData() {
-    //   Preventing CORS errors
-    const proxy = "https://cors-anywhere.herokuapp.com/";
-    const api = `${proxy}https://api.darksky.net/forecast/ee3193faf8920bcf3d01cfdbdb9d14ad/42.3601,-71.0589,255657600?exclude=currently,flags`;
-
-    const response = await fetch(api);
-    const data = await response.json();
-
-    if(data){
-      setWeather(data);
-      // return (data);
-    }else{
-      setErrors(console.log("Error! Can not fetch data")) 
+    this.state = {
+      time: null,
     }
   }
 
-  useEffect(() => {
-    fetchData();
-  });
+  componentDidMount() {
+     //   Preventing CORS errors
+     async function fetchData() {
+      const proxy = "https://cors-anywhere.herokuapp.com/";
+      const api = `${proxy}https://api.darksky.net/forecast/ee3193faf8920bcf3d01cfdbdb9d14ad/42.3601,-71.0589,255657600?exclude=currently,flags`;
+  
+      const response = await fetch(api);
+      const data = await response.json();
+  
+      if(data){
+        // console.log(data.daily.data["0"]);
+        const weatherData = data.daily.data["0"];
+        const {time, summary, icon, pressure, windspeed, humidity} = weatherData;
+      }else{
+        alert("Error! Can not fetch data"); 
+      }
+    }
+    
+    return fetchData();
 
-  return (
-    <div>
-      <CurrentWeather />
-      {/* {summary} */}
-      {console.log(weather.daily)}
-      <hr />
-      <span>Has error: {JSON.stringify(hasError)}</span>
-    </div>
-  );
-};
+  }
+
+   render(){
+     return(
+       <div>
+        <div></div>
+       </div>
+     )
+   }
+}
+
+
+   
 export default Weather;
